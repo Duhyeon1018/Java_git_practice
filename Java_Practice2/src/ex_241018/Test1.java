bc8cd117cad8cdf8bfb9a7187a6b27a7256a79a7
Duhyeon Kang <rkdengus1234@naver.com>
2024-10-21 09:11:10 +0900

    first commit

---
+++ Java_Practice2/src/ex_241018/Test1.java
@@ -0,0 +1,17 @@
+package ex_241018;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int resultInt = Hello.sum(10, 20);
+		System.out.println("결과 resultInt" + resultInt);
+
+		
+		
+	}
+	
+
+}
